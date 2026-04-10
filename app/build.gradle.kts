a028edbdd92d89a0ebfb84d62c521ae8874b8bfb
MobileDelita2SIA <delita24si@mahasiswa.pcr.ac.id>
2026-04-10 08:32:31 +0700

    Revert "Pertemuan 3 penerapan binding"
    This reverts commit af4e9c44c0a1a5003302bb31151d2dde55e23f73.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,9 +31,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures{
-        viewBinding = true
-    }
 }
 
 dependencies {
